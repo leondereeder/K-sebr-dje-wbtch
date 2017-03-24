bc5994f8ba3ba14724adc1497534bebc354e8cce
KNiemeijer <k.j.niemeijer@students.uu.nl>
2017-03-24 13:11:23 +0100

    Verkeerde img

--- koleon/public/javascripts/winkelmandje.js
+++ koleon/public/javascripts/winkelmandje.js
@@ -12,17 +12,17 @@ function activateModal(id, price) {
     case "Alien17":
         modalheader.innerHTML = "Alienware 17";
         modalbody.innerHTML = "Enorm beeldscherm. Overweldigende grafische prestaties. De Alienware 17 is de perfecte gaminglaptop voor kracht en prestaties. Voeg de optionele Graphics Amplifier toe om de ervaring nog beter te maken.<br><br>Prijs: €1299-";
-        modalimg.src = "img/alienware17.png";
+        modalimg.src = "images/alienware17.png";
         break;
     case "MSI5":
         modalheader.innerHTML = "MSI Hunter 5";
         modalbody.innerHTML = "De GL72 notebook van MSI is een krachtige notebook, ideaal voor zowel multimedia als gaming. Dankzij de laatste Intel Core i7 processor, 8 GB geheugen en een harde schijf van 1 TB is deze snelle notebook een echte alleskunner. Deze notebook is ook voorzien van de nieuwste NVIDIA GTX960M videokaart en voorzien van Windows 10. Kortom deze notebook heeft alles in huis voor de complete game-ervaring!<br><br>Prijs: €999-";
-        modalimg.src = "img/msi.png";
+        modalimg.src = "images/msi.png";
         break;
     case "PredX":
         modalheader.innerHTML = "Predator X";
         modalbody.innerHTML = "De Predator X brengt je game-ervaring tot ongekende hoogtes dankzij de nieuwste Intel-i7 processor en gtx 1080 videokaart. Samen met een full HD scherm en 16 gigabyte RAM geheugen kan niks je stoppen.<br><br>Prijs: €1499-";
-        modalimg.src = "img/PredatorX.png";
+        modalimg.src = "images/PredatorX.png";
         break;
     }
 
@@ -68,7 +68,7 @@ function displayInCart(id, price) {
         $("#afrekenen").css("display", "block");
         $("#productentabel").find("tbody").append(
             "<tr>" +
-            "<td><img src=\"img/alienware17.png\" alt=\"Alienware 17\">" +
+            "<td><img src=\"images/alienware17.png\" alt=\"Alienware 17\">" +
             "<br>Alienware 17" +
             "<td>Enorm beeldscherm. Overweldigende grafische prestaties. De Alienware 17 is de perfecte gaminglaptop voor kracht en prestaties." +
             "Voeg de optionele Graphics Amplifier toe om de ervaring nog beter te maken." +
@@ -83,7 +83,7 @@ function displayInCart(id, price) {
         $("#afrekenen").css("display", "block");
         $("#productentabel").find("tbody").append(
             "<tr>" +
-            "<td><img src=\"img/msi.png\" alt=\"MSI 5\">" +
+            "<td><img src=\"images/msi.png\" alt=\"MSI 5\">" +
             "<br>MSI Hunter 5" +
             "<td>De GL72 notebook van MSI is een krachtige notebook, ideaal voor zowel multimedia als gaming." +
             "Dankzij de laatste Intel Core i7 processor, 8 GB geheugen en een harde schijf van 1 TB is deze snelle notebook een echte alleskunner." +
@@ -100,7 +100,7 @@ function displayInCart(id, price) {
         $("#afrekenen").css("display", "block");
         $("#productentabel").find("tbody").append(
             "<tr>" +
-            "<td><img src=\"img/PredatorX.png\" alt=\"PredatorX\">" +
+            "<td><img src=\"images/PredatorX.png\" alt=\"PredatorX\">" +
             "<br>Predator X" +
             "<td>De Predator X brengt je game-ervaring tot ongekende hoogtes dankzij de nieuwste Intel-i7 processor en gtx 1080 videokaart." +
             "Samen met een full HD scherm en 16 gigabyte RAM geheugen kan niks je stoppen." +
