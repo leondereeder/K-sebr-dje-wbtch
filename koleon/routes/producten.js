3e1d3c0ba901dca735762cd3004f13c09cc25311
KNiemeijer <k.j.niemeijer@students.uu.nl>
2017-03-30 12:40:32 +0200

    Eerste poging productpagina

--- koleon/routes/producten.js
+++ koleon/routes/producten.js
@@ -1,21 +1,26 @@
 var express = require('express');
 var router = express.Router();
 var sqlite3 = require('sqlite3').verbose();
+var data = [];
 
 router.get('/', function(req, res, next) {
-  res.render('producten', { title: 'Producten' });
+	loadDB();
+  res.render('producten', { products: data });
 });
 
-router.post('/', function(req, res, next) {
-	loadDB();
-});
 module.exports = router;
 
 function loadDB() {
-	var db = new sqlite3.Database('public/protected/db.sqlite3');
-	// DB logic
-	db.each("SELECT product_name AS productName, price AS price, image AS imageString FROM Product", function(err, row) {
-		
-	});
-	db.close();
+	var x = data.length;
+	if (x==0) {
+		var db = new sqlite3.Database('public/protected/db.sqlite3');
+		// DB logic
+		db.each("SELECT ProductID AS productID, ProductName AS productName, Description AS description, Stock AS stock, Price AS price, Image as image FROM PRODUCTS", function(err, row) {
+			//console.log(row.ProductID + " | " + row.ProductName + " | " + row.Description + " | " + row.Stock);
+			data.push(row);
+			console.log(data[x]);
+		});
+		console.log(data.length);
+		db.close();
+	}
 }
