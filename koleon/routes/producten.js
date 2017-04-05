bd1252e8875b1346238071e4c4113f4c360d0802
leondereeder <leondereeder@gmail.com>
2017-04-05 15:28:53 +0200

    Revert "as"
    This reverts commit 760cc12386c25f2e18775beebde17cadd45789b5.

--- koleon/routes/producten.js
+++ koleon/routes/producten.js
@@ -15,8 +15,7 @@ function loadDB() {
 	if (x==0) {
 		var db = new sqlite3.Database('public/protected/db.sqlite3');
 		// DB logic
-		
-		db.each("SELECT ProductID AS productID, ProductName AS productName, Description AS description, Stock AS stock, Price AS price, Image as image FROM PRODUCTS ORDER BY Price ASC", function(err, row) {
+		db.each("SELECT ProductID AS productID, ProductName AS productName, Description AS description, Stock AS stock, Price AS price, Image as image FROM PRODUCTS", function(err, row) {
 			data.push(row);
 			console.log(data[x]);
 		});
